a3604b600cc55a82967aff91089e07d363cb7483
MaLeVoLenT1 <dionemrobertson@gmail.com>
2021-05-21 00:28:03 -0400

    Added Javascript code and Mix code.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,9 +11,25 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.browserSync('localhost/thelux');
+mix.browserSync('localhost/thelux/public');
 
 mix.setResourceRoot('../');
+
+mix.js('resources/js/scripts.js', 'public/js');
+
+mix.scripts([
+    'resources/js/lib/visible.js',
+    'resources/js/lib/menuzord.js',
+    'resources/js/lib/jquery.nav.js',
+    'resources/js/lib/wow.min.js',
+    'resources/js/lib/owl.carousel.min.js',
+    'resources/js/lib/smooth.js',
+    'resources/js/lib/bootstrap-filestyle.min.js',
+    'resources/js/lib/jquery.ui.touch-punch.min.js',
+    'resources/js/lib/jquery.fixedheadertable.js',
+], 'public/js/vendor.js');
+
+mix.sass('resources/sass/landing.scss', 'public/css');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
